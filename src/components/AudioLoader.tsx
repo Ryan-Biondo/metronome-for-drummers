662125141f6f1eeee5070b96bfc4e21af8e77842
sqallFF <jlewis110wssu@gmail.com>
2023-09-23 08:11:18 -0600

    Updated AudioLoad to Functional Component

--- src/components/AudioLoader.tsx
+++ src/components/AudioLoader.tsx
@@ -1,16 +1,32 @@
-export default class AudioLoader {
-    buffers: AudioBuffer[]
+import useMetronomeStore from '../store';
+// export default class AudioLoader {
+//     buffers: AudioBuffer[]
 
-    constructor(context: AudioContext, urls: string[]) {
-        this.buffers = []
+//     constructor(context: AudioContext, urls: string[]) {
+//         this.buffers = []
 
-        const promises: Promise<AudioBuffer>[] = urls.map((url: string) =>
-            fetch(url)
-                .then((response: Response) => response.arrayBuffer())
-                .then((arrayBuffer: ArrayBuffer) => context.decodeAudioData(arrayBuffer)))
+//         const promises: Promise<AudioBuffer>[] = urls.map((url: string) =>
+//             fetch(url)
+//                 .then((response: Response) => response.arrayBuffer())
+//                 .then((arrayBuffer: ArrayBuffer) => context.decodeAudioData(arrayBuffer)))
+
+//         Promise.all(promises)
+//             .then((buffers: Awaited<AudioBuffer>[]) => this.buffers = buffers)
+//             .catch(error => console.error(error))
+//     }
+// }
+
+export default function AudioLoader() {
+    const {urls, setBuffers} = useMetronomeStore();
+    const context = new AudioContext
+
+    const promises:Promise<AudioBuffer>[] = urls.map((url: string) =>
+        fetch(url)
+        .then((res: Response) => res.arrayBuffer())
+        .then((arrayBuffer: ArrayBuffer) => context.decodeAudioData(arrayBuffer)))
 
         Promise.all(promises)
-            .then((buffers: Awaited<AudioBuffer>[]) => this.buffers = buffers)
-            .catch(error => console.error(error))
-    }
+            .then((buffers: Awaited<AudioBuffer>[]) => setBuffers(buffers))
+            .catch(err => console.error(err))
+
 }
