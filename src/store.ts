597aabd73dd443dc8032834e97af49fc2eedcdc4
sqallFF <jlewis110wssu@gmail.com>
2023-09-23 07:15:58 -0600

    Added BufferState and Set Buffers to Met Store

--- src/store.ts
+++ src/store.ts
@@ -4,18 +4,22 @@ interface MetronomeState {
     bpm: number;
     beatsPerMeasure: number;
     isPlaying: boolean;
+    buffers: Array<object>;
     setBPM: (newBPM: number) => void;
     setBeats: (newBeats: number) => void;
     togglePlay: () => void;
+    setBuffers: (newBuffers: Array<object>) => void;
   }
   
   const useMetronomeStore = create<MetronomeState>((set) => ({
     bpm: 120,
     beatsPerMeasure: 4,
     isPlaying: false,
+    buffers: [],
     setBPM: (newBPM) => set({ bpm: newBPM }),
     setBeats: (newBeats) => set({ beatsPerMeasure: newBeats }),
     togglePlay: () => set((state) => ({ isPlaying: !state.isPlaying })),
+    setBuffers: (newBuffers) => set({ buffers: newBuffers})
   }));
   
   export default useMetronomeStore;
