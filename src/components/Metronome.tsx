1df66386cc5cb56b5a99b0806b4ef64ceeccccd6
sqallFF <jlewis110wssu@gmail.com>
2023-10-04 16:10:45 -0600

    removed MetStore and Components

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -1,13 +1,9 @@
 import BeatsControl from './BeatsControl';
 import BpmControl from './BpmControl';
 import ControlButtons from './ControlButtons';
-// import MetronomeSound from './MetronomeSound';
-// import SnareSound from './audio/snare-drum-808-3.wav'
-import useMetronomeStore from '../store';
+
 
 const Metronome = () => {
-  const {buffers} = useMetronomeStore();
-  console.log(buffers)
   return (
     <>
       <div className="app-container">
